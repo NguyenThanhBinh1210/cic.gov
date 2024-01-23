299813b18a7e6ecc779afe7c2d0b78bb0a169e2a
catalina5 <thanhbinh12369yt@gmail.com>
2024-01-23 11:22:23 +0700

    binh

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -131,7 +131,7 @@ const Settings = () => {
               className='text-white flex items-center gap-2 bg-[#007bff] hover:bg-blue-600 transition-all h-max focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 :bg-blue-600 :hover:bg-[#007bff] focus:outline-none :focus:ring-blue-800'
             >
               <svg
-                className='w-4 h-4 inline-block text-gray-800 dark:text-white'
+                className='w-4 h-4 inline-block text-white'
                 aria-hidden='true'
                 xmlns='http://www.w3.org/2000/svg'
                 fill='currentColor'
